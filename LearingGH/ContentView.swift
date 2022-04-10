64027d3c3ad0d29744aa32bb2a1d69dc4ebbb3e8
Rajinder GIll <rajinder-gill@hotmail.com>
2022-04-09 18:54:55 -0700

    Initial Commit

---
+++ LearingGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearingGH
+//
+//  Created by Rajinder Gill on 2022-04-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearingGH/LearingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGHApp.swift
+//  LearingGH
+//
+//  Created by Rajinder Gill on 2022-04-09.
+//
+
+import SwiftUI
+
+@main
+struct LearingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
